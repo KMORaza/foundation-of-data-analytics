a2a1abf929bf8580a608ae10374a6469ffac7e72
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 10:41:58 +0530

    Q6.r

---
+++ Lab3/Q6.r
@@ -0,0 +1,6 @@
+# Create vector "temperatures"
+temperatures <- c(25, 30, 28, 32, 29)
+# Find the maximum temperature
+max_temperature <- max(temperatures)
+# Print the result
+print(max_temperature)
