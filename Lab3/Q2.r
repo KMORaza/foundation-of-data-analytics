85a0f3d681154f783d1a9cd79cb77d81b727e1dd
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 10:39:21 +0530

    Q2.r

---
+++ Lab3/Q2.r
@@ -0,0 +1,8 @@
+# Create the prices vector
+prices <- c(10, 15, 20, 25, 30)
+# Create the quantities vector
+quantities <- c(2, 4, 6, 8, 10)
+# Calculate the total cost
+total_cost <- prices * quantities
+# Print the total cost
+print(total_cost)
