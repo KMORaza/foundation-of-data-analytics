669c577180e46e497d5b67cea6742780229753ce
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 09:28:28 +0530

    Vectors.r

---
+++ Lab1/Vectors.r
@@ -0,0 +1,6 @@
+numeric_vector <- c(1, 2, 3, 4, 5)
+character_vector <- c("red", "green", "blue")
+logical_vector <- c(TRUE, FALSE, TRUE)
+print(numeric_vector)
+print(character_vector)
+print(logical_vector)
