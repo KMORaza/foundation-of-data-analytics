35a08dd8b67dde4c4e6f32baa1e2620fc00015e8
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 10:40:07 +0530

    Q3.r

---
+++ Lab3/Q3.r
@@ -0,0 +1,10 @@
+# Create vector1 and vector2
+vector1 <- c(1, 2, 3)
+vector2 <- c(4, 5, 6)
+# Concatenate vector1 and vector2
+combined_vector <- c(vector1, vector2)
+# Calculate the length of combined_vector
+length_combined <- length(combined_vector)
+# Print the combined vector and its length
+print(combined_vector)
+print(length_combined)
