b9eac94a9caa4e675b496a969cd6f1ab5276c9a7
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 09:24:04 +0530

    LogicalOperators.r

---
+++ Lab1/LogicalOperators.r
@@ -0,0 +1,16 @@
+x <- TRUE
+y <- FALSE
+# Logical AND
+andResult <- x & y
+print(andResult)
+# Logical OR
+orResult <- x | y
+print(orResult)
+# Logical NOT
+notResultX <- !x
+notResultY <- !y
+print(notResultX)
+print(notResultY)
+# Logical XOR
+xorResult <- xor(x, y)
+print(xorResult)
