ab5895c71536a005b906c00d06065fb923715ba0
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 10:50:41 +0530

    Q15.r

---
+++ Lab3/Q15.r
@@ -0,0 +1,21 @@
+# Create two numeric vectors
+vec1 <- c(2, 4, 6, 8)
+vec2 <- c(3, 5, 7, 9)
+# Addition
+addition <- vec1 + vec2
+print(addition)
+# Subtraction
+subtraction <- vec1 - vec2
+print(subtraction)
+# Multiplication
+multiplication <- vec1 * vec2
+print(multiplication)
+# Division
+division <- vec1 / vec2
+print(division)
+# Exponentiation
+exponentiation <- vec1 ^ vec2
+print(exponentiation)
+# Modulo
+modulo <- vec1 %% vec2
+print(modulo)
