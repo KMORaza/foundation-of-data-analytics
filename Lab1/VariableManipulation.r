35909e0ea7b0d36053209cc863f00fb735f88636
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 09:22:07 +0530

    VariableManipulation.r

---
+++ Lab1/VariableManipulation.r
@@ -0,0 +1,27 @@
+# Variable assignment
+x <- 5
+y <- "Hello"
+z <- TRUE
+# Display variable values
+x
+y
+z
+# Arithmetic operations
+a <- x + 3
+b <- sqrt(a)
+# Concatenation
+greeting <- paste(y, "World!")
+# Logical operations
+is_positive <- a > 0
+is_equal <- a == b
+# Data transformation
+upper_greeting <- toupper(greeting)
+# Vector creation
+numbers <- c(1, 2, 3, 4, 5)
+# Accessing vector elements
+second_number <- numbers[2]
+# Data frame creation
+df <- data.frame(Name = c("John", "Emily", "Michael"), Age = c(25, 30, 35))
+# Accessing data frame columns
+names <- df$Name
+ages <- df$Age
