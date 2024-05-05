ad23fe5a6abe33449d396fd863afdd627c37bc3b
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 09:23:13 +0530

    ComparisonOperators.r

---
+++ Lab1/ComparisonOperators.r
@@ -0,0 +1,20 @@
+x <- 5
+y <- 3
+# Equal to
+isEqual <- x == y
+print(isEqual)
+# Not equal to
+isNotEqual <- x != y
+print(isNotEqual)
+# Greater than
+isGreaterThan <- x > y
+print(isGreaterThan)
+# Less than
+isLessThan <- x < y
+print(isLessThan)
+# Greater than or equal to
+isGreaterThanOrEqual <- x >= y
+print(isGreaterThanOrEqual)
+# Less than or equal to
+isLessThanOrEqual <- x <= y
+print(isLessThanOrEqual)
