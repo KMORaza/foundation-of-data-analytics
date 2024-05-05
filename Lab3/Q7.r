87186414e97748071581e55db7d12fafbda95bcd
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 10:42:27 +0530

    Q7.r

---
+++ Lab3/Q7.r
@@ -0,0 +1,6 @@
+# Create logical vector "logical_vector"
+logical_vector <- c(TRUE, FALSE, TRUE, TRUE)
+# Check if all elements are TRUE
+all_true <- all(logical_vector)
+# Print the result
+print(all_true)
