35d217ce88c6cca651503bd95967e615568b2e40
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 09:20:58 +0530

    CheckingVariables.r

---
+++ Lab1/CheckingVariables.r
@@ -0,0 +1,16 @@
+# Create variables
+x <- 5
+y <- "Hello, world!"
+z <- c(1, 2, 3, 4, 5)
+# Check variable types
+print(class(x)) # Check type of variable x
+print(class(y)) # Check type of variable y
+print(class(z)) # Check type of variable z
+# Check variable values
+print(x) # Print value of variable x
+print(y) # Print value of variable y
+print(z) # Print value of variable z
+# Check variable attributes
+print(attributes(x)) # Check attributes of variable x
+print(attributes(y)) # Check attributes of variable y
+print(attributes(z)) # Check attributes of variable z
