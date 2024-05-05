f24908ed1e10671fca4da9344156a4d7a3205ed8
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 10:43:37 +0530

    Q9.r

---
+++ Lab3/Q9.r
@@ -0,0 +1,6 @@
+# Create character vector "fruits"
+fruits <- c("apple", "banana", "orange", "grape", "mango")
+# Sort the vector in alphabetical order
+sorted_fruits <- sort(fruits)
+# Print the sorted vector
+print(sorted_fruits)
