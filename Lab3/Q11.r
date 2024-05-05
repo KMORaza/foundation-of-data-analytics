77888487efc70424fe6ff58076361b6a2ac0b606
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 10:44:44 +0530

    Q11.r

---
+++ Lab3/Q11.r
@@ -0,0 +1,11 @@
+# Create vector "names"
+
+names <- c("John", "Jane", "James", "Julia")
+
+# Extract the first two elements
+
+first_two <- names[1:2]
+
+# Print the extracted elements
+
+print(first_two)
