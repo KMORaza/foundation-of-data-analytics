82901bb73a0258de777b4b94e76621394db2628f
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 09:19:28 +0530

    ArithmeticOperations.r

---
+++ Lab1/ArithmeticOperations.r
@@ -0,0 +1,20 @@
+# Addition
+a <- 10
+b <- 5
+sum <- a + b
+print(sum)
+# Subtraction
+diff <- a - b
+print(diff)
+# Division
+div <- a / b
+print(div)
+# Multiplication
+mult <- a * b
+print(mult)
+# Modulo (remainder)
+mod <- a %% b
+print(mod)
+# Exponentiation
+exp <- a ^ b
+print(exp)
