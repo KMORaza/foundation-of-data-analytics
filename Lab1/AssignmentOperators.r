6013e8aa7e0f8d269c0437be5a3fd776cc5e08d8
Raza <141234596+KMORaza@users.noreply.github.com>
2024-05-05 09:20:15 +0530

    AssignmentOperators.r

---
+++ Lab1/AssignmentOperators.r
@@ -0,0 +1,17 @@
+x <- 5
+y <- x + 3
+# Assignment Operators
+x <- 10 # x is assigned the value 10
+print(x)
+x <- x + 3 # x is incremented by 3 and assigned the result
+print(x)
+x <- x - 2 # x is decremented by 2 and assigned the result
+print(x)
+x <- x * 2 # x is multiplied by 2 and assigned the result
+print(x)
+x <- x / 3 # x is divided by 3 and assigned the result
+print(x)
+x <- x %% 4 # x is divided by 4 and the remainder is assigned
+print(x)
+x <- x ^ 2 # x is raised to the power of 2 and assigned the result
+print(x)
